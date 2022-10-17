f6fd0b9f087d893736abc152b0165b17f4acb3c5
Mohammed Azharuddin <108989254+azhar-005@users.noreply.github.com>
2022-10-17 19:35:14 +0530

    did

--- Rest Parameter/index.js
+++ Rest Parameter/index.js
@@ -1,12 +1,12 @@
 // console.log("This is JS file for the parameter");
-// // Rest Parameter//
-// // A rest parameter allows a function to accept indefinite parameter and stores in an array
-// // It must be in the last // isko last me hi likh skte hain
-// // It introduced in ECMA 6 (ES-6)
-// const RestParameter = (a, b, ...data) => {
-// console.log(data);
-// }
-// RestParameter(10,20,30,40,50,60,70);
+// Rest Parameter//
+// A rest parameter allows a function to accept indefinite parameter and stores in an array
+// It must be in the last // isko last me hi likh skte hain
+// It introduced in ECMA 6 (ES-6)
+const RestParameter = (a, b, ...data) => {
+console.log(data);
+}
+RestParameter(10,20,30,40,50,60,70);
 
 // Template Literal//
 // It came in ES6
